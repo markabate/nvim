3a2dea7fd2fb89139f035d6d8f04f3fbc3e0df7d
markabate <mabate@utexas.edu>
2026-07-08 11:44:21 -0700

    Neovim config initial commit

---
+++ after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+-- Only apply these options to the current Python buffer
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true
+
---
+++ init.lua
@@ -0,0 +1,122 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	-- Syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+
+	-- LSP support
+	{
+		"neovim/nvim-lspconfig",
+	},
+
+	-- Autocomplete
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
+
+	-- LSP manager
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	{
+		"williamboman/mason-lspconfig.nvim",
+	},
+
+	{
+		"tpope/vim-fugitive",
+	},
+
+	{
+		"airblade/vim-gitgutter",
+	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+
+	{
+		"preservim/nerdtree",
+	},
+
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+})
+
+require("nvim-treesitter").setup({
+	highlight = {
+		enable = true,
+	},
+})
+
+local cmp = require("cmp")
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+})
+
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"pyright",
+		"jdtls",
+		"clangd",
+		"asm_lsp",
+	},
+})
+
+
+-- ### Keymaps ###
+vim.keymap.set("n", "om", ":lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set("n", "ln", ":set nu<CR>")
+vim.keymap.set("n", "nn", ":set nonu<CR>")
+
+-- NERDTree
+vim.keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>")
+
+-- Tabs
+vim.keymap.set("n", "<C-l>", ":tabmove +1<CR>")
+vim.keymap.set("n", "<C-r>", ":tabmove -1<CR>")
+
+-- Settings
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+
+vim.opt.number = true
