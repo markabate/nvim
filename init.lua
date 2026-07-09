7a47881beac95941798deb875136d54ae6dd89aa
markabate <mabate@utexas.edu>
2026-07-09 13:28:35 -0700

    Update tabs keymappings

--- init.lua
+++ init.lua
@@ -109,9 +109,9 @@ vim.keymap.set("n", "nn", ":set nonu<CR>")
 vim.keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>")
 
 -- Tabs
-vim.keymap.set("n", "<C-t>", ":tabnew<CR>")
-vim.keymap.set("n", "<C-r>", ":tabmove +1<CR>")
-vim.keymap.set("n", "<C-l>", ":tabmove -1<CR>")
+vim.keymap.set("n", "<C-w>n", ":tabnew<CR>")
+vim.keymap.set("n", "<C-w>r", ":tabmove +1<CR>")
+vim.keymap.set("n", "<C-w>l", ":tabmove -1<CR>")
 
 -- Settings
 vim.opt.tabstop = 4
